6bf49d22d6f47d0f10b24a67eeff89cfb1fb1b9a
F. Souzza <filipesouzza.adm@gmail.com>
2022-06-12 18:23:15 -0300

    add skills qualif services + css + js

--- assets/js/portf.js
+++ assets/js/portf.js
@@ -1,3 +1,5 @@
+
+// ACTIONS SKILLS
 const skillsContent = document.getElementsByClassName('skills__content'),
 skillsHeader = document.querySelectorAll('.skills__header')
 
@@ -16,4 +18,30 @@ function toggleSkills(){
 
 skillsHeader.forEach((el) => {
     el.addEventListener('click', toggleSkills)
+})
+
+
+
+
+// ACTIONS QUALIFICATION TABS
+const tabs = document.querySelectorAll('[data-target]'),
+tabContents = document.querySelectorAll('[data-content]')
+
+
+tabs.forEach(tab =>{
+    tab.addEventListener('click',()=>{
+        const target = document.querySelector(tab.dataset.target)
+
+        tabContents.forEach(tabContent =>{
+            tabContent.classList.remove('qualification__active')
+        })
+
+        target.classList.add('qualification__active')
+
+        tabs.forEach(tab =>{
+            tab.classList.remove('qualification__active')
+        })
+
+        tab.classList.add('qualification__active')
+    })
 })
