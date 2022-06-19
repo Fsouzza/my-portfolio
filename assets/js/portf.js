7866fd72c164c39301a3d87fc8454046c007ee8b
F. Souzza <filipesouzza.adm@gmail.com>
2022-06-19 16:35:46 -0300

    add page portfolio

--- assets/js/portf.js
+++ assets/js/portf.js
@@ -45,3 +45,17 @@ tabs.forEach(tab =>{
         tab.classList.add('qualification__active')
     })
 })
+
+// ACTIONS PORTFOLIO SWIPEE
+let swiper = new Swiper('.portfolio__container', {
+    cssMode: true,
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    }
+  });
